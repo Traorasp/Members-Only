a4a7a007d472d511b9eb319ad7e71c9cabfb9e1d
Traorasp <miguelsolanopaz@outlook.com>
2022-08-29 18:03:32 -0400

    Securely added database uri

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+mongoose.connect(process.env.mongoUri, {useNewUrlParser:true, useUnifiedTopology:true});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoSB connection error:"));
 
 var app = express();
 
