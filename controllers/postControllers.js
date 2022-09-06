f446431be280a8be56687deffd2e04603de53f3c
Traorasp <miguelsolanopaz@outlook.com>
2022-09-05 22:01:58 -0400

    Added working create post view

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,5 +1,7 @@
 const Post = require('../models/Post');
 
+const {body, validationResult} = require('express-validator');
+
 exports.load_posts = (req, res) => {
     if(res.locals.currentUser === undefined) {
         res.redirect('/catalog/signin');
@@ -7,6 +9,7 @@ exports.load_posts = (req, res) => {
     }
     Post.find()
     .sort({"date" : -1})
+    .populate('author')
     .exec(function(err, posts) {
         if(err) {return next(err);}
         res.render("post_lists", {posts: posts});
@@ -14,12 +17,43 @@ exports.load_posts = (req, res) => {
 };
 
 exports.create_posts_get = (req, res) => {
-    res.send('Not implemented');
+    if(res.locals.currentUser === undefined) {
+        res.redirect('/catalog/signin');
+        return;
+    }
+    res.render('create_post');
 };
 
-exports.create_posts_post = (req, res) => {
-    res.send('Not implemented');
-};
+exports.create_posts_post = [
+    body('title', "Post must have a title")
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    body('message', "Post must have a message")
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.render('create_post', {post: req.body, errors: errors.array()})
+            return;
+        }
+        console.log(res.locals.currentUser._id)
+        const post = new Post({
+            title: req.body.title,
+            author: res.locals.currentUser._id,
+            message: req.body.message,
+        })
+        console.log(post)
+        post.save((err) => {
+            if(err) {
+                return next(err);
+            }
+            res.redirect('/catalog/posts');
+        })
+    }
+]
 
 exports.delete_posts_get = (req, res) => {
     res.send('Not implemented');
--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
     title: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
     message: {type: String, required: true},
     date: {type: Date, default: Date.now()},
 });
